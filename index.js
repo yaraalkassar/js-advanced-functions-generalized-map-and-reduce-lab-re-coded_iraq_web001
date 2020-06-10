9b1b76accecefd71e814638d3c026ce05c843aeb
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-10 00:29:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add your functions here
+function map(array, functions) {
+  let newArr=[]
+   array.forEach(el=>{
+     newArr.push(functions(el))
+   })
+   return newArr
+}
+function reduce(array, functions, startingPoint) {
+
+}
